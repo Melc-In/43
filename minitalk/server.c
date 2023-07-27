fd9da7dec06b5c529b32942887852049672820c7
Melc <80842427+Melc-In@users.noreply.github.com>
2023-07-27 08:16:57 +0200

    seg tlauf

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: cglandus <cglandus@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 15:19:13 by cglandus          #+#    #+#             */
-/*   Updated: 2023/07/27 04:48:40 by cglandus         ###   ########.fr       */
+/*   Updated: 2023/07/27 08:12:02 by cglandus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,28 @@ int	get_len_mess(int signum)
 	bit_len++;
 	if (bit_len == 31)
 	{
-		byte << 1;
-		byte != (signum == SIGUSR1);
-		return (byte);
+		byte_len <<= 1;
+		byte_len |= (signum == SIGUSR1);
+		return (byte_len);
 	}
+	return (bit_len);
 }
 
 void	s_handler(int signum)
 {
 	static char	*message;
-	static int	len_mess;
+	static size_t	len_mess;
 	static int	byte = 0;
 	static int	bit = 0;
 	static int	i = 0;
 
 	bit++;
-	if (ft_strlen(message) == len_message)
+	if (ft_strlen(message) == len_mess)
 	{
 		ft_putstr_fd(message, 1);
 		bit = 42;
 	}
-	if (bit <= 31);
+	if (bit <= 31)
 	{
 		len_mess = get_len_mess(signum);
 		message = ft_calloc(len_mess, 1);
@@ -48,8 +49,8 @@ void	s_handler(int signum)
 	}
 	if (bit == 40)
 	{
-		byte << 1;
-		byte != (signum == SIGUSR1)
+		byte <<= 1;
+		byte |= (signum == SIGUSR1);
 		message[i] = byte;
 		byte = 0;
 		bit = 32;
