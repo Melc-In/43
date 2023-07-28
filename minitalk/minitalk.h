86394425bb9465f26c54db3ade954256ae3d9194
Melc <80842427+Melc-In@users.noreply.github.com>
2023-07-28 09:15:52 +0200

    Add files via upload

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: cglandus <cglandus@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 15:09:53 by cglandus          #+#    #+#             */
-/*   Updated: 2023/07/28 07:23:50 by cglandus         ###   ########.fr       */
+/*   Updated: 2023/07/28 09:02:24 by cglandus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 # include "libft/libft.h"
 # include <signal.h>
 
-//typedef struct s_msg
-//{
-//	char	*message;
-//	int		mess_len;
-//	int		byte;
-//	int		bit;
-//	int		i;
-//
-//}	t_msg;
+typedef struct s_msg
+{
+	char	*message;
+	int		len_mess;
+	int		byte;
+	int		bit;
+	int		i;
+
+}	t_msg;
 
 #endif
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,54 +6,47 @@
 /*   By: cglandus <cglandus@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 15:19:13 by cglandus          #+#    #+#             */
-/*   Updated: 2023/07/28 07:24:11 by cglandus         ###   ########.fr       */
+/*   Updated: 2023/07/28 09:12:12 by cglandus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int	get_len_mess(int signum)
-{
-	static int	byte_len = 0;
-	static int	bit_len = 0;
-
-	byte_len <<= 1;
-	byte_len |= (signum == SIGUSR1);
-	bit_len++;
-	if (bit_len == 31)
-		return (byte_len);
-	return (0);
-}
-
 static void	s_handler(int signum)
 {
 	static char	*message;
-	static int	len_mess;
+	static int	len_mess = 0;
+	static int	bit_len = 0;
 	static int	byte = 0;
 	static int	bit = 0;
 	static int	i = 0;
 
-	if (bit <= 31)
+	byte <<= 1;
+	byte |= (signum == SIGUSR1);
+	bit_len++;
+	if (bit_len == 31)
 	{
-		len_mess = get_len_mess(signum);
-		if (len_mess != 0)
-			message = ft_calloc(len_mess + 1, 1);
-	}
-	if (bit >= 32)
-	{
-		byte <<= 1;
-		byte |= (signum == SIGUSR1);
-	}
-	if (bit == 40)
-	{
-		if (byte == '\0')
-			ft_putstr_fd(message, 1);
-		message[i] = byte;
+		len_mess = byte;
+		message = ft_calloc(len_mess + 1, 1);
 		byte = 0;
-		bit = 32;
-		i++;
 	}
-	bit++;
+	if (bit_len > 31)
+		bit++;
+	if (bit == 7)
+	{
+		message[i] = byte;
+		i++;
+		if (byte == '\0')
+		{
+			ft_putstr_fd(message, 1);
+			free(message);
+			bit_len = 0;
+			len_mess = 0;
+			i = 0;
+		}
+		byte = 0;
+		bit = 0;
+	}
 }
 
 //static void	s_handler(int signum)
@@ -64,7 +57,7 @@ static void	s_handler(int signum)
 int	main(void)
 {
 	ft_putnbr_fd(getpid(), 1);
-	ft_putchar('\n');
+	ft_putchar_fd('\n', 1);
 	while (1)
 	{
 		signal(SIGUSR1, s_handler);
