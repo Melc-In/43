c06cebdc466ec136847856814ad86afdb5ed300a
Melc <80842427+Melc-In@users.noreply.github.com>
2023-11-13 17:35:14 +0100

    small rainbows

--- push_swap/parse.c
+++ push_swap/parse.c
@@ -6,34 +6,11 @@
 /*   By: cglandus <cglandus@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 23:37:22 by cglandus          #+#    #+#             */
-/*   Updated: 2023/11/13 17:19:56 by cglandus         ###   ########.fr       */
+/*   Updated: 2023/11/13 17:34:08 by cglandus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-//static int	check_stack(s_list stack)
-//{
-//}
-
-static int	quote_handler(char *arg, size_t i)
-{
-	char	*splited_arg;
-	size_t	i;
-
-	i = -1;
-	splited_arg = ft_strtrim(arg, "\"");
-	splited_arg = ft_split(arg, arg[i + 1]);
-	if (!splited_arg)
-		return (0);
-	while (arg[i++])
-	{
-		if (!ft_isdigit(arg[i]))
-			return (0);
-	}
-	ft_putstr_fd(splited_arg, 1);
-	return (1);
-}
 
 static int	check_args(char *arg)
 {
@@ -68,16 +45,11 @@ static int	is_valid(char *arg)
 	return (0);
 }
 
-static int	no_quotes(char *arg)
+static int	no_quotes(int digit)
 {
-	int	digit;
-
-	if (!)
-		return (1);
-	
-	digit = ft_atoi(arg);
-	//check si c un nombre signé ou pas
+	ft_lstnew(digit);
 	// + check si il est pas deja dans ma pile
+	// l'ajoute a la stack
 }
 
 int	parsing(int ac, char **args)
