e01ee91155c9ecec9b7e7e3d4487d7e8c4e709c8
Melc <cglandus@student.42angouleme.fr>
2024-01-31 19:28:47 +0100

    optimisation ongoing

--- push_swap/sort_tools.c
+++ push_swap/sort_tools.c
@@ -6,7 +6,7 @@
 /*   By: cglandus <cglandus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 18:19:46 by cglandus          #+#    #+#             */
-/*   Updated: 2024/01/30 22:51:33 by cglandus         ###   ########.fr       */
+/*   Updated: 2024/01/31 19:28:05 by cglandus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,8 +53,22 @@ static size_t	chunk_switch(size_t size)
 	if (size <= 500)
 		return (9);
 	if (size > 500)
-		return (9);
+		return (12);
 	return (5);
+}
+
+size_t	to_push(t_stack s, size_t min, size_t max)
+{
+	size_t	i;
+
+	i = s.size - 1;
+	while (i >= 0)
+	{
+		if ((size_t)s.nums[i] >= min && (size_t)s.nums[i] <= max)
+			return (i);
+		i--;
+	}
+	return (i);
 }
 
 static void	push_chunk(t_stack *a, t_stack *b, size_t chunk_size, size_t i)
@@ -63,9 +77,20 @@ static void	push_chunk(t_stack *a, t_stack *b, size_t chunk_size, size_t i)
 	{
 		if (i * chunk_size <= (size_t)a->nums[a->size - 1]
 			&& (size_t)a->nums[a->size - 1] <= (i + 1) * chunk_size)
-			push(a, b, "pb\n");
-		else
+		{
+			if ((size_t)a->nums[a->size - 1] >= chunk_size * (i + 1) / 2)
+				push(a, b, "pb\n");
+			else
+			{
+				push(a, b, "pb\n");
+				rrab(b, "rrb\n");
+			}
+		}
+		else if (to_push(*a, chunk_size * i, chunk_size * (i + 1))
+				>= a->size / 2)
 			rotate(a, "ra\n");
+		else
+			rrab(a, "rra\n");
 	}
 }
 
@@ -86,7 +111,10 @@ void	butterfly_sort(t_stack *a, t_stack *b)
 	{
 		while (get_max(*b) != b->size - 1)
 		{
-			rotate(b, "rb\n");
+			if (get_max(*b) >= b->size / 2)
+				rotate(b, "rb\n");
+			else
+				rrab(b, "rrb\n");
 		}
 		push(b, a, "pa\n");
 	}
