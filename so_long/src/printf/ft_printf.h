c3d96c3649818158dd924ab05ec669b7cb70d38a
Melc <cglandus@student.42angouleme.fr>
2024-02-12 20:38:25 +0100

    begining Makefile

---
+++ so_long/src/printf/ft_printf.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cglandus <cglandus@student.42angoulem      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 01:57:45 by cglandus          #+#    #+#             */
+/*   Updated: 2022/12/20 22:53:25 by cglandus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <limits.h>
+
+int		absol(int n);
+void	ft_bzero(void *s, size_t n);
+int		ft_putchar_fd(char c, int fd);
+int		ft_putnbr_base_fd(unsigned long long nbr, char *base, int fd);
+int		ft_putnbr_fd(int n, int fd);
+int		ft_putstr_fd(char *s, int fd);
+int		print_hex(unsigned int n, char c);
+int		print_ints(int n);
+int		print_ptr(unsigned long long ptr);
+int		print_uints(unsigned int n);
+int		ft_printf(const char *str, ...);
+
+#endif
